97bc75fe29b822b325e4c02ca5806e0d02d2e552
Hao Duong <howarddful123@gmail.com>
2021-12-11 15:54:31 -0800

    puck will be at random position after a goal and temp fix for arm rotation

--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -46,13 +46,22 @@ public class Puck : MonoBehaviour
     public void ResetPuckAndPlayer()
     {
         if (slapshot) slapshot = false;
-        _theWorld.ResetPlayerPosition();
+        //_theWorld.ResetPlayerPosition();
     }
     public void ResetPuck()
     {
         slapshot = false;
         _positionStart.y = 0.05f;
         transform.localPosition = _positionStart;
+        transform.rotation = Quaternion.identity;
+        _rigidBody.velocity = Vector3.zero;
+    }
+
+    public void ResetPuckRandom()
+    {
+        slapshot = false;
+        _positionStart.y = 0.05f;
+        transform.localPosition = new Vector3(Random.Range(-4f, 4f) , _positionStart.y, Random.Range(0f, 6f));
         transform.rotation = Quaternion.identity;
         _rigidBody.velocity = Vector3.zero;
     }
