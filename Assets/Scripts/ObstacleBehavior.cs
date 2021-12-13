4268e9134408ef8301e485850e52be2264ca8e95
ryscco <74153923+ryscco@users.noreply.github.com>
2021-12-13 09:19:33 -0800

    goal mesh + texture

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -1,13 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class ObstacleBehavior : MonoBehaviour
 {
     int direction = 1;
-    // Start is called before the first frame update
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
